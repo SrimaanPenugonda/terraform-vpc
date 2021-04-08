b7e19892427c14ca1ee1e0051c47f0f603569483
Srimaan Penugonda <p.srimaan10@gmail.com>
2021-04-08 09:23:22 +0530

    Added outputs.tf which has subnet information

--- terraform-vpc/outputs.tf
+++ terraform-vpc/outputs.tf
@@ -1,6 +1,7 @@
 //This output file is to pass private and public subnet information to autoscaling grp
-//this outputs sits in state file and further asg access the vpc state file and read the subnet information
-//
+//this outputs sits in state file and further asg module access the vpc state file
+//and read the subnet information
+
 output "PUBLIC_SUBNETS" {
   value = aws_subnet.public-subnets.*.id
 }
